be7357c9855640d83f0d1ff13a06ef3fc1d7ba4b
Iurii <galchenkou@gmail.com>
2018-06-18 15:16:54 +0300

    HW

---
+++ lesson_7/Yoga_project/js/script.js
@@ -0,0 +1,37 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+	let tab = document.getElementsByClassName('info-header-tab'),
+		tabContent = document.getElementsByClassName('info-tabcontent'),
+		info = document.getElementsByClassName('info-header')[0];
+
+	function hideTabContent(num) {
+		for (let i = num; i < tabContent.length; i++) {
+			tabContent[i].classList.remove('show');
+			tabContent[i].classList.add('hide');
+		};
+	};
+
+	hideTabContent(1);
+
+	function showTabContent(num) {
+		if (tabContent[num].classList.contains('hide')) {
+			hideTabContent(0);
+			tabContent[num].classList.remove('hide');
+			tabContent[num].classList.add('show');
+		}
+	};
+
+	info.addEventListener('click', function(event) {
+		let target = event.target;
+		if (target.className == 'info-header-tab') {
+			for (let i = 0; i < tab.length; i++) {
+				if (target == tab[i]) {
+					showTabContent(i);
+					break;
+				};
+			};
+		};
+	});
+
+
+});
---
+++ lesson_7/timer_project/js/main.js
@@ -0,0 +1,32 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+	let timer = document.querySelector('.timer');
+
+	function showTime() {
+		let currentDate,
+			hours,
+			minutes,
+			seconds;
+
+		currentDate = new Date();
+		hours = check(currentDate.getHours());
+		minutes = check(currentDate.getMinutes());
+		seconds = check(currentDate.getSeconds());
+		timer.innerHTML = `${hours}:${minutes}:${seconds}`;
+	};
+
+	function check(num) {
+		let tempArr = ('' + num).split(''),
+			tempNum = num;
+		if (tempArr.length == 1) {
+			//tempNum = +( (tempArr.splice(0, 0, '0')).join(''));
+			tempArr.unshift('0');
+			return tempArr.join('');
+		} else {
+			return tempNum;
+		}
+	};
+
+	setInterval(showTime, 1000);
+
+});
